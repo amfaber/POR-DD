1b44c01c92d19dd4bced49f13f75d56fad6aad27
Andreas Malthe Faber <andreas.mfaber@gmail.com>
2022-06-06 08:12:50 +0200

    making project root available in futils

--- python_scripts/futils/__init__.py
+++ python_scripts/futils/__init__.py
@@ -1,7 +1,2 @@
-# import os
-# import importlib
-# print(__file__)
-# for file in os.listdir(os.path.dirname(__file__)):
-#     if file == "__init__.py" or not file.endswith(".py"):
-#         continue
-#     importlib.import_module(file[:-3])
+from os.path import dirname
+prj_root = dirname(dirname(dirname(__file__)))
