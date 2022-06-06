c261ab98336eb89f072a07132425929227623b8e
qzj517 <qzj517@a00552.science.domain>
2022-06-06 07:50:06 +0200

    py3dmol

--- python_scripts/futils/comparison_util.py
+++ python_scripts/futils/comparison_util.py
@@ -1,7 +1,7 @@
 import scipy.stats
 import matplotlib.pyplot as plt
 import numpy as np
-def Linreg(x, y, ax = None, coefpos = (0.8, 0.85), corpos = (0.2, 0.1), coef = False):
+def Linreg(x, y, ax = None, coefpos = (0.8, 0.85), corpos = (0.2, 0.1), coef = False, fontsize = 20):
     if ax is None:
         ax = plt.gca()
     ax.plot(x, y, ".")
@@ -9,12 +9,13 @@ def Linreg(x, y, ax = None, coefpos = (0.8, 0.85), corpos = (0.2, 0.1), coef = F
     spearmanr = scipy.stats.mstats.spearmanr(x, y)[0]
     xlims = np.array(ax.get_xlim())
     ys = linreg[0]*xlims+linreg[1]
+
     ax.plot(xlims, ys)
 
     if coef:
         txt = "$y=m x + b$\n"
         txt += f"$m = {linreg[0]:.3g}$\n$b = {linreg[1]:.3g}$"
-        ax.text(*coefpos, txt, transform = ax.transAxes, va = "center", ha = "center")
+        ax.text(*coefpos, txt, transform = ax.transAxes, va = "center", ha = "center", fontsize = fontsize)
     
     txt = f"Pearson's r = {linreg[2]:.3g}"#\nspearmans r = {spearmanr:.3g}"
-    ax.text(*corpos, txt, transform = ax.transAxes, ha = "center")
+    ax.text(*corpos, txt, transform = ax.transAxes, ha = "center", fontsize = fontsize)
