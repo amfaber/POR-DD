67e9c83a7cb5afdcfe573bff9c6d3098ee695ec5
amfaber <andreas.mfaber@gmail.com>
2022-06-20 11:23:08 +0200

    LIGS and PROT in futils

--- python_scripts/futils/__init__.py
+++ python_scripts/futils/__init__.py
@@ -1,2 +1,10 @@
-from os.path import dirname
-prj_root = dirname(dirname(dirname(__file__)))
+from pathlib import Path
+from os.path import relpath
+_script_dir = Path(__file__).parent
+_cwd = Path.cwd()
+PROT_ABS = "../../data/raw_data/por_structures/3ES9_1_reduced.pdb"
+LIGS_ABS = "../../data/raw_data/cyp_screen/test_3D_opt_1216.sdf"
+LIGS_ABS = str((_script_dir / LIGS_ABS).resolve())
+PROT_ABS = str((_script_dir / PROT_ABS).resolve())
+LIGS = relpath(LIGS_ABS, _cwd)
+PROT = relpath(PROT_ABS, _cwd)
