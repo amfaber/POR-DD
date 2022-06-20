745e27bbcbf6fa5e87505856bc579be8528fdea9
amfaber <andreas.mfaber@gmail.com>
2022-06-20 11:27:48 +0200

    renaming and cleaning

--- experiment_scripts/seed_experiment.py
+++ experiment_scripts/seed_experiment.py
@@ -1,10 +1,10 @@
-import equibind_inference 
+import multiligand_inference 
 if __name__ == "__main__": 
     def make_args(seed):
         return ["-l", "/home/qzj517/POR-DD/data/raw_data/cyp_screen/test_3D_opt_1216.sdf",
-    "-r", "/home/qzj517/POR-DD/data/raw_data/cyp_screen/protein.pdb", "-o",
+    "-r", "../data/raw_data/por_structures/3ES9_bioassem_1.pdb", "-o",
     f"/home/qzj517/POR-DD/data/equibind_processed/seed_experiment/seed_{seed}", 
     "--n_workers_data_load", "4", "--batch_size", "32", "--no_skip", "--seed", f"{seed}"]
 
     for i in range(20):
-        equibind_inference.main(make_args(i))
+        multiligand_inference.main(make_args(i))
