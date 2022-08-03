9b6371674d6370dcf46b394dc8e96a39ae68156f
amfaber <andreas.mfaber@gmail.com>
2022-08-03 09:30:07 +0200

    CLI rust parser

--- any_metric_parser/src/main.rs
+++ any_metric_parser/src/main.rs
@@ -9,27 +9,29 @@ use any_metric_parser::utils;
 struct Args {
     input_dir: path::PathBuf,
     output_dir: path::PathBuf,
+
     #[clap(short, long, default_value = "15")]
     inner_threads: usize,
+    
     #[clap(short, long, default_value = "8")]
     outer_threads: usize,
-    #[clap(short, long)]
+    
+    #[clap(short, long, multiple = true)]
     targets: Option<Vec<String>>,
-    #[clap(short, long, default_value = "vsscore")]
+    
+    #[clap(short, long, default_value = "vsscore", possible_values = vec!["vsscore", "cnnaffinity", "cnnscore", "vina"])]
     compare_type: String
 }
 
 fn main() -> Result<(), io::Error> {
     let args = Args::parse();
+    println!("{:?}", args);
     let input_dir = args.input_dir;
     let output_dir = args.output_dir;
     let inner_threads = args.inner_threads;
     let outer_threads = args.outer_threads;
     let targets = args.targets;
     let compare_type = args.compare_type;
-
-    // let input_dir = path::PathBuf::from("/home/qzj517/POR-DD/data/gnina_LIT-PCBA_VS_data/lit-pcba");
-    // let output_dir = path::PathBuf::from("/home/qzj517/POR-DD/data/gnina_LIT-PCBA_VS_data/test");
     utils::parse_all_targets_in_dir(input_dir, output_dir, inner_threads, outer_threads, &targets, &compare_type)?;
     Ok(())
 }
